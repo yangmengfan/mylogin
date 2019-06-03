17bc0e4f487c565450072bbc018ca4e657716372
yangmfa <yangmfa@dcholdings.com>
2019-06-03 19:43:49 +0800

    freemaker自动生成代码示例

---
+++ src/main/java/cn/myplay/util/FreemarkerDemo.java
@@ -0,0 +1,51 @@
+package cn.myplay.util;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Auther: ymfa
+ * @Date: 2019/6/3 19:06
+ * @Description:
+ */
+public class FreemarkerDemo {
+    private static final String TEMPLATE_PATH = "F:/备份";
+    private static final String CLASS_PATH = "F:/备份";
+
+    public static void main(String[] args) {
+        // step1 创建freeMarker配置实例
+        Configuration configuration = new Configuration();
+        Writer out = null;
+        try {
+            // step2 获取模版路径
+            configuration.setDirectoryForTemplateLoading(new File(TEMPLATE_PATH));
+            // step3 创建数据模型
+            Map<String, Object> dataMap = new HashMap<String, Object>();
+            dataMap.put("classPath", "com.freemark.hello");
+            dataMap.put("className", "AutoCodeDemo");
+            dataMap.put("helloWorld", "通过简单的 <代码自动生产程序> 演示 FreeMarker的HelloWorld！");
+            // step4 加载模版文件
+            Template template = configuration.getTemplate("hello.ftl");
+            // step5 生成数据
+            File docFile = new File(CLASS_PATH + "\\" + "AutoCodeDemo.java");
+            out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(docFile)));
+            // step6 输出文件
+            template.process(dataMap, out);
+            System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^AutoCodeDemo.java 文件创建成功 !");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != out) {
+                    out.flush();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+}
