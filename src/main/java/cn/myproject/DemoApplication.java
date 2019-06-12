53652476ac30f2321f45e69e25cf4283314dc091
yangmfa <yangmfa@dcholdings.com>
2019-06-12 10:29:19 +0800

    修改包名

---
+++ src/main/java/cn/myproject/DemoApplication.java
@@ -0,0 +1,15 @@
+package cn.myproject;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("cn.myproject.mapper")
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cn/myproject/mapper/RoleMapper.java
@@ -0,0 +1,16 @@
+package cn.myproject.mapper;
+
+import cn.myproject.entity.Role;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author yangmf
+ * @since 2019-06-11
+ */
+public interface RoleMapper extends BaseMapper<Role> {
+
+}
